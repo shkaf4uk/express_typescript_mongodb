9a9234f42d92a352ca0419299b01f24b3acb8773
Maksym Shafarenko <35640383+shkaf4uk@users.noreply.github.com>
2022-05-03 10:20:12 +0300

    Delete dist directory

--- dist/config/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.configs = void 0;
-exports.configs = {
-    secret: 'random_secret_key',
-    port: 8080
-};
-//# sourceMappingURL=index.js.map
--- dist/schemas/list-users.schema.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=list-users.schema.js.map
